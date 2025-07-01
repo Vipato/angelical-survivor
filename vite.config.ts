6a0b1708c96b8efa86d13ffd743bab5919227b22
Antonio Vitor <vipayoman.201820192020@gmail.com>
2025-07-01 20:03:18 -0300

    Configura a base para o GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/angelical-survivor/', // <--- ESSA É A LINHA IMPORTANTE
   plugins: [react()],
 })
